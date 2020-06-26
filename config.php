15c9a81191b828d314ed5355ecd9f3fe25130644
Kieran Briggs <kbriggs@chartered.college>
2020-06-26 14:49:27 +0100

    Stub files for theme
    Config.php
    lib.php
    version.php
    language file

---
+++ config.php
@@ -0,0 +1,57 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Chartered College of Teaching 2021 theme.
+ *
+ * @package    theme_charteredcollege
+ * @copyright  2020 Kieran Briggs
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// This line protects the file from being accessed by a URL directly.
+defined('MOODLE_INTERNAL') || die();
+
+$THEME->name = 'charteredcollege';
+
+// Left Blank as this theme uses scss
+$THEME->sheets = [];
+
+// Needed for TinyMCE only
+$THEME->editor_sheets = [];
+
+$THEME->parents = ['boost'];
+
+$THEME->scss = 'main';
+// A dock is a way to take blocks out of the page and put them in a persistent floating area on the side of the page. Boost
+// does not support a dock so we won't either - but look at bootstrapbase for an example of a theme with a dock.
+$THEME->enable_dock = false;
+
+// This is an old setting used to load specific CSS for some YUI JS. We don't need it in Boost based themes because Boost
+// provides default styling for the YUI modules that we use. It is not recommended to use this setting anymore.
+$THEME->yuicssmodules = array();
+
+// Most themes will use this rendererfactory as this is the one that allows the theme to override any other renderer.
+$THEME->rendererfactory = 'theme_overridden_renderer_factory';
+
+// This is a list of blocks that are required to exist on all pages for this theme to function correctly. For example
+// bootstrap base requires the settings and navigation blocks because otherwise there would be no way to navigate to all the
+// pages in Moodle. Boost does not require these blocks because it provides other ways to navigate built into the theme.
+$THEME->requiredblocks = '';
+
+// This is a feature that tells the blocks library not to use the "Add a block" block. We don't want this in boost based themes because
+// it forces a block region into the page when editing is enabled and it takes up too much room.
+$THEME->addblockposition = BLOCK_ADDBLOCK_POSITION_FLATNAV;
